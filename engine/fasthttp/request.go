10472380397dc1d0d4dc713f41ae19a838ebafbc
Marcin Tojek <tojek.m@gmail.com>
2016-06-09 18:36:37 +0200

    merging

--- engine/fasthttp/request.go
+++ engine/fasthttp/request.go
@@ -6,10 +6,10 @@ import (
 	"bytes"
 	"io"
 	"mime/multipart"
-)
 
-import (
+	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine"
+	"github.com/labstack/echo/log"
 	"github.com/valyala/fasthttp"
 )
 
@@ -17,14 +17,25 @@ type (
 	// Request implements `engine.Request`.
 	Request struct {
 		*fasthttp.RequestCtx
-		url    engine.URL
 		header engine.Header
+		url    engine.URL
+		logger log.Logger
 	}
 )
 
-// TLS implements `engine.Request#TLS` function.
-func (r *Request) TLS() bool {
-	return r.IsTLS()
+// NewRequest returns `Request` instance.
+func NewRequest(c *fasthttp.RequestCtx, l log.Logger) *Request {
+	return &Request{
+		RequestCtx: c,
+		url:        &URL{URI: c.URI()},
+		header:     &RequestHeader{RequestHeader: &c.Request.Header},
+		logger:     l,
+	}
+}
+
+// IsTLS implements `engine.Request#TLS` function.
+func (r *Request) IsTLS() bool {
+	return r.RequestCtx.IsTLS()
 }
 
 // Scheme implements `engine.Request#Scheme` function.
@@ -47,6 +58,16 @@ func (r *Request) Header() engine.Header {
 	return r.header
 }
 
+// Referer implements `engine.Request#Referer` function.
+func (r *Request) Referer() string {
+	return string(r.Request.Header.Referer())
+}
+
+// ContentLength implements `engine.Request#ContentLength` function.
+func (r *Request) ContentLength() int64 {
+	return int64(r.Request.Header.ContentLength())
+}
+
 // UserAgent implements `engine.Request#UserAgent` function.
 func (r *Request) UserAgent() string {
 	return string(r.RequestCtx.UserAgent())
@@ -64,7 +85,7 @@ func (r *Request) Method() string {
 
 // SetMethod implements `engine.Request#SetMethod` function.
 func (r *Request) SetMethod(method string) {
-	r.Request.Header.SetMethod(method)
+	r.Request.Header.SetMethodBytes([]byte(method))
 }
 
 // URI implements `engine.Request#URI` function.
@@ -72,9 +93,19 @@ func (r *Request) URI() string {
 	return string(r.RequestURI())
 }
 
+// SetURI implements `engine.Request#SetURI` function.
+func (r *Request) SetURI(uri string) {
+	r.Request.Header.SetRequestURI(uri)
+}
+
 // Body implements `engine.Request#Body` function.
 func (r *Request) Body() io.Reader {
-	return bytes.NewBuffer(r.PostBody())
+	return bytes.NewBuffer(r.Request.Body())
+}
+
+// SetBody implements `engine.Request#SetBody` function.
+func (r *Request) SetBody(reader io.Reader) {
+	r.Request.SetBodyStream(reader, 0)
 }
 
 // FormValue implements `engine.Request#FormValue` function.
@@ -85,10 +116,21 @@ func (r *Request) FormValue(name string) string {
 // FormParams implements `engine.Request#FormParams` function.
 func (r *Request) FormParams() (params map[string][]string) {
 	params = make(map[string][]string)
-	r.PostArgs().VisitAll(func(k, v []byte) {
-		// TODO: Filling with only first value
-		params[string(k)] = []string{string(v)}
-	})
+	mf, err := r.RequestCtx.MultipartForm()
+
+	if err == fasthttp.ErrNoMultipartForm {
+		r.PostArgs().VisitAll(func(k, v []byte) {
+			// TODO: Filling with only first value
+			params[string(k)] = []string{string(v)}
+		})
+	} else if err == nil {
+		for k, v := range mf.Value {
+			if len(v) > 0 {
+				params[k] = v
+			}
+		}
+	}
+
 	return
 }
 
@@ -100,6 +142,30 @@ func (r *Request) FormFile(name string) (*multipart.FileHeader, error) {
 // MultipartForm implements `engine.Request#MultipartForm` function.
 func (r *Request) MultipartForm() (*multipart.Form, error) {
 	return r.RequestCtx.MultipartForm()
+}
+
+// Cookie implements `engine.Request#Cookie` function.
+func (r *Request) Cookie(name string) (engine.Cookie, error) {
+	c := new(fasthttp.Cookie)
+	b := r.Request.Header.Cookie(name)
+	if b == nil {
+		return nil, echo.ErrCookieNotFound
+	}
+	c.ParseBytes(b)
+	c.SetKey(name)
+	return &Cookie{c}, nil
+}
+
+// Cookies implements `engine.Request#Cookies` function.
+func (r *Request) Cookies() []engine.Cookie {
+	cookies := make([]engine.Cookie, 0)
+	r.Request.Header.VisitAllCookie(func(name, value []byte) {
+		c := new(fasthttp.Cookie)
+		c.SetKey(string(name))
+		c.ParseBytes(value)
+		cookies = append(cookies, &Cookie{c})
+	})
+	return cookies
 }
 
 func (r *Request) reset(c *fasthttp.RequestCtx, h engine.Header, u engine.URL) {
--- middleware/basic_auth_test.go
+++ middleware/basic_auth_test.go
@@ -12,45 +12,39 @@ import (
 
 func TestBasicAuth(t *testing.T) {
 	e := echo.New()
-	rq := test.NewRequest(echo.GET, "/", nil)
-	rs := test.NewResponseRecorder()
-	c := echo.NewContext(rq, rs, e)
+	req := test.NewRequest(echo.GET, "/", nil)
+	res := test.NewResponseRecorder()
+	c := e.NewContext(req, res)
 	f := func(u, p string) bool {
 		if u == "joe" && p == "secret" {
 			return true
 		}
 		return false
 	}
-	h := BasicAuth(f)(echo.HandlerFunc(func(c echo.Context) error {
+	h := BasicAuth(f)(func(c echo.Context) error {
 		return c.String(http.StatusOK, "test")
-	}))
+	})
 
 	// Valid credentials
 	auth := basic + " " + base64.StdEncoding.EncodeToString([]byte("joe:secret"))
-	rq.Header().Set(echo.Authorization, auth)
-	assert.NoError(t, h.Handle(c))
-
-	//---------------------
-	// Invalid credentials
-	//---------------------
+	req.Header().Set(echo.HeaderAuthorization, auth)
+	assert.NoError(t, h(c))
 
 	// Incorrect password
 	auth = basic + " " + base64.StdEncoding.EncodeToString([]byte("joe:password"))
-	rq.Header().Set(echo.Authorization, auth)
-	he := h.Handle(c).(*echo.HTTPError)
+	req.Header().Set(echo.HeaderAuthorization, auth)
+	he := h(c).(*echo.HTTPError)
 	assert.Equal(t, http.StatusUnauthorized, he.Code)
-	assert.Equal(t, basic+" realm=Restricted", rs.Header().Get(echo.WWWAuthenticate))
+	assert.Equal(t, basic+" realm=Restricted", res.Header().Get(echo.HeaderWWWAuthenticate))
 
 	// Empty Authorization header
-	rq.Header().Set(echo.Authorization, "")
-	he = h.Handle(c).(*echo.HTTPError)
+	req.Header().Set(echo.HeaderAuthorization, "")
+	he = h(c).(*echo.HTTPError)
 	assert.Equal(t, http.StatusUnauthorized, he.Code)
-	assert.Equal(t, basic+" realm=Restricted", rs.Header().Get(echo.WWWAuthenticate))
 
 	// Invalid Authorization header
 	auth = base64.StdEncoding.EncodeToString([]byte("invalid"))
-	rq.Header().Set(echo.Authorization, auth)
-	he = h.Handle(c).(*echo.HTTPError)
+	req.Header().Set(echo.HeaderAuthorization, auth)
+	he = h(c).(*echo.HTTPError)
 	assert.Equal(t, http.StatusUnauthorized, he.Code)
-	assert.Equal(t, basic+" realm=Restricted", rs.Header().Get(echo.WWWAuthenticate))
 }
